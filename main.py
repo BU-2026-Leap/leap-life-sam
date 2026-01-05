98b233baff4e48ceb70060a1912881bdce98f9c3
skochis814 <skochis@bu.edu>
2026-01-05 15:20:46 -0500

    edits to main.py and test_main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import csv
 import os
 import json
+from pathlib import Path
 
-input_filename = "test_scores.csv"
+full_base_path = Path(__file__).resolve().parent
+input_filename = full_base_path / "test_scores.csv"
 output_filename = "output.json"
 
 if os.path.exists(output_filename):
