60fc547dd6f59db607e993e53924d695828449c5
skochis814 <skochis@bu.edu>
2026-01-05 12:23:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ if os.path.exists(output_filename):
 
 average_final = 0.0
 unique_students = 0
+total_scores = 0
+final_count = 0
 
 with open(input_filename) as f:
     reader = csv.DictReader(f)
@@ -18,7 +20,14 @@ with open(input_filename) as f:
 
         # TODO: compute average final score
 
+        if row["exam_name"] == "final":
+            total_scores = total_scores + float(row["score"])
+            final_count = final_count + 1
         # TODO: unique student count
+
+    unique_students = final_count
+    average_final = (total_scores / unique_students)
+
 
 if os.path.exists(output_filename):
     os.remove(output_filename)
